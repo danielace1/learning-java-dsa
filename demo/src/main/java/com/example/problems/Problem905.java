5db6881200e3d72fed46e073588984e80d7c8988
danielace1 <danielsudharsan1805@gmail.com>
2026-07-03 22:42:03 +0530

    problems

---
+++ demo/src/main/java/com/example/problems/Problem905.java
@@ -0,0 +1,27 @@
+package com.example.problems;
+
+// https://leetcode.com/problems/sort-array-by-parity
+public class Problem905 {
+    class Solution {
+        public int[] sortArrayByParity(int[] nums) {
+            int l = 0;
+            int r = nums.length - 1;
+
+            while (l < r) {
+                if (nums[l] % 2 > nums[r] % 2) {
+                    int temp = nums[l];
+                    nums[l] = nums[r];
+                    nums[r] = temp;
+                }
+
+                if (nums[l] % 2 == 0)
+                    l++;
+                if (nums[r] % 2 == 1)
+                    r--;
+            }
+
+            return nums;
+        }
+
+    }
+}
