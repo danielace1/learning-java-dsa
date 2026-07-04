445d10c8c8dc52dbb4e9ae8b9a912e352d6c0998
danielace1 <danielsudharsan1805@gmail.com>
2026-07-04 19:41:26 +0530

    problems

---
+++ demo/src/main/java/com/example/problems/Problem238.java
@@ -0,0 +1,23 @@
+package com.example.problems;
+
+// https://leetcode.com/problems/product-of-array-except-self/
+public class Problem238 {
+
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return res;
+    }
+}
