0ffd5db02311d5b483039d72539b2c48110ae333
danielace1 <danielsudharsan1805@gmail.com>
2026-07-06 02:18:31 +0530

    problems

---
+++ demo/src/main/java/com/example/problems/Problem191.java
@@ -0,0 +1,15 @@
+package com.example.problems;
+
+// https://leetcode.com/problems/number-of-1-bits/
+public class Problem191 {
+    class Solution {
+        public int hammingWeight(int n) {
+            int count = 0;
+            while (n > 0) {
+                count++;
+                n = n & (n - 1);
+            }
+            return count;
+        }
+    }
+}
