c23a72b7994997b5e27bb98890d2e79aeb4092d5
danielace1 <danielsudharsan1805@gmail.com>
2026-08-11 18:58:38 +0530

    learning

--- demo/src/main/java/com/example/problems/ArrangeEvenOdd.java
+++ demo/src/main/java/com/example/problems/ArrangeEvenOdd.java
@@ -68,7 +68,58 @@ public class ArrangeEvenOdd {
 
         }
 
-        System.out.println(Arrays.toString(ans));
+        // System.out.println(Arrays.toString(ans));
+        // System.out.println(Arrays.toString(arr));
+
+        arrangeOnDescOddEven(new int[] { 12, 34, 45, 9, 8, 90, 3 });
+
+    }
+
+    private static void arrangeOnDescOddEven(int[] arr) {
+        int l = 0;
+        int r = arr.length - 1;
+
+        while (l < r) {
+            if (arr[l] % 2 == 0) {
+                l++;
+            } else if (arr[r] % 2 != 0) {
+                r--;
+            } else {
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+            }
+
+        }
+
+        // odd num start
+        int split = 0;
+        while (split < arr.length && arr[split] % 2 == 0) {
+            split++;
+        }
+
+        // sort evens in desc
+        for (int i = 0; i < split - 1; i++) {
+            for (int j = 0; j < split - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // sort odd in desc
+        for (int i = split; i < arr.length - 1; i++) {
+            for (int j = split; j < arr.length - (i - split) - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
         System.out.println(Arrays.toString(arr));
 
     }
