edcfb2a4bc06f729a79f238732f6582f4e8d839b
danielace1 <danielsudharsan1805@gmail.com>
2026-08-17 22:46:11 +0530

    learning

---
+++ demo/src/main/java/com/example/problems/leetcode/misc/Problem2029.java
@@ -0,0 +1,31 @@
+package com.example.problems.leetcode.misc;
+
+// https://leetcode.com/problems/stone-game-ix
+public class Problem2029 {
+    class Solution {
+        public boolean stoneGameIX(int[] stones) {
+            int count0 = 0;
+            int count1 = 0;
+            int count2 = 0;
+
+            for (int stone : stones) {
+                int r = stone % 3;
+
+                if (r == 0) {
+                    count0++;
+                } else if (r == 1) {
+                    count1++;
+                } else {
+                    count2++;
+                }
+            }
+
+            if (count0 % 2 == 0) {
+                return count1 > 0 && count2 > 0;
+            }
+
+            return Math.abs(count1 - count2) > 2;
+
+        }
+    }
+}
