5439175eff92683d3c3a6225bcf0b3e149594a66
danielace1 <danielsudharsan1805@gmail.com>
2026-09-18 23:23:39 +0530

    learning

---
+++ demo/src/main/java/com/databases/Problem197.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/rising-temperature/
+
+
+select w1.id from Weather w1 
+join Weather w2
+    on w1.recordDate=Date_add(w2.recordDate, interval 1 day)
+where w1.temperature > w2.temperature;
